5795201ab7b58699c3e3306cbf4d432c2b3d2e8e
Mert Güvençli <guvenclimert@gmail.com>
2022-04-26 00:17:59 +0300

    ui added

---
+++ social-otter/ui/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 style={{fontSize: '240px'}}>🦦</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
